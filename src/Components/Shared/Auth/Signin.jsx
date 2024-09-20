9f78b59b36cb0f7e598d9901062ff2e5ee980b46
venom <abdo30201080101694@gmail.com>
2024-09-20 08:37:47 +0300

    add the signin form

---
+++ src/Components/Shared/Auth/Signin.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+// Components/SignInForm.js
+export default function SignInForm() {
+
+  return (
+    <form action="#" className="sign-in-form" onSubmit={handleFormSubmit}>
+      <h2 className="title">Sign in</h2>
+      <div className="input-field">
+        <i className="fas fa-envelope"></i>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+
+        />
+      </div>
+      <div className="input-field">
+        <i className="fas fa-lock"></i>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
+      <input type="submit" value="Login" className="btn solid" />
+    </form>
+  );
+}
