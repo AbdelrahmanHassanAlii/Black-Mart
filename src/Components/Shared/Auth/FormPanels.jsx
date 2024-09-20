baa69bdded6e6e4097bd04d0ecb16334ef360030
venom <abdo30201080101694@gmail.com>
2024-09-20 08:41:02 +0300

    add the panel form

---
+++ src/Components/Shared/Auth/FormPanels.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+export default function FormPanels({ handleSignUpClick, handleSignInClick }) {
+  return (
+    <div className="panels-container">
+      <div className="panel left-panel">
+        <div className="content">
+          <h3>Black Mart</h3>
+          <p>
+            Discover a world of possibilities! Join us and explore a vibrant
+            community where ideas flourish and connections thrive.
+          </p>
+          <button
+            className="btn transparent"
+            id="sign-up-btn"
+            onClick={handleSignUpClick}
+          >
+            Sign up
+          </button>
+        </div>
+        <img
+          src="https://i.ibb.co/6HXL6q1/Privacy-policy-rafiki.png"
+          className="image"
+          alt=""
+        />
+      </div>
+      <div className="panel right-panel">
+        <div className="content">
+          <h3>Black Mart</h3>
+          <p>
+            Thank you for being part of our System. Your presence enriches our
+            shared experiences. Let's continue this journey together!
+          </p>
+          <button
+            className="btn transparent"
+            id="sign-in-btn"
+            onClick={handleSignInClick}
+          >
+            Sign in
+          </button>
+        </div>
+        <img
+          src="https://i.ibb.co/nP8H853/Mobile-login-rafiki.png"
+          className="image"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
