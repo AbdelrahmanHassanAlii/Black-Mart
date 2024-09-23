40dd8d0a71a1faadbdef029be8ee750748ab5a14
venom <abdo30201080101694@gmail.com>
2024-09-23 22:46:02 +0300

    handle the basics of sign form

--- src/Components/Shared/Auth/Signup.jsx
+++ src/Components/Shared/Auth/Signup.jsx
@@ -1,36 +1,79 @@
 /* eslint-disable react/prop-types */
+
+import { useState } from "react";
+
 /* eslint-disable no-unused-vars */
 export default function SignUpForm({ handleSignInClick }) {
+  const [userData, setUserData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(userData);
   };
   return (
     <form action="#" className="sign-up-form" onSubmit={handleFormSubmit}>
       <h2 className="form-title">Sign up</h2>
       <div className="input-field">
         <i className="fas fa-signature"></i>
-        <input type="text" name="firstName" placeholder="First Name" />
-      </div>
-      <div className="input-field">
-        <i className="fas fa-signature"></i>
-        <input type="text" name="lastName" placeholder="Last Name" />
+        <input
+          type="text"
+          name="userName"
+          placeholder="User Name"
+          onChange={handleChange}
+        />
       </div>
       <div className="input-field email">
         <div className="left">
           <i className="fas fa-envelope"></i>
-          <input type="text" name="email" placeholder="Email" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
         </div>
-        <div className="right">@blackmart</div>
+        <div className="right">@blackmart.com</div>
       </div>
       <div className="input-field">
         <i className="fas fa-lock"></i>
-        <input type="password" name="password" placeholder="Password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
       </div>
       <div className="input-field">
-        <i className="fas fa-phone"></i>
-        <input type="text" name="phoneNumber" placeholder="Phone Number" />
+        <i className="fas fa-lock"></i>
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          onChange={handleChange}
+        />
       </div>
-      <input type="submit" className="btn" value="Sign up" />
+      <input
+        type="submit"
+        className="btn"
+        value="Sign up"
+        onClick={handleFormSubmit}
+      />
     </form>
   );
 }
