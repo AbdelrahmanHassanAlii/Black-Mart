12d8d6e39b1dfd98c3d6105ccf8e5cfbac4785af
unknown <abdo30201080101694@gmail.com>
2024-09-25 15:58:16 +0300

    add three apis of brands

---
+++ src/Helper/Apis/Admin/Brand/addBrand.js
@@ -0,0 +1,12 @@
+export const addBrand = async (brand) => {
+    const url = "http://localhost:3000/api/v1/brand";
+
+    const response = await axios.post(url, brand, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            "token": `${getToken()}`,
+        },
+    });
+
+    return response;
+}
