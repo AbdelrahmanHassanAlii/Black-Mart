35b33bd56c098b2c38abc0f0a39fb67d16897d62
venom <abdo30201080101694@gmail.com>
2024-10-11 00:34:12 +0300

    handle delete category SWAL

--- src/Components/Admin/CouponeCard.jsx
+++ src/Components/Admin/CouponeCard.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import style from "../../assets/CSS/Admin/CouponeCard.module.css";
-import { RiDiscountPercentFill } from "react-icons/ri";
 import { FaPercent } from "react-icons/fa";
 
 export default function CouponeCard({ coupon }) {
